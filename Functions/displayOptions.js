6179d8f09dd0a53fe73aaf108f89d31cd5a70e84
Sebastian Siarczynski <sebastian.siarczynski@gmail.com>
2020-06-15 22:22:49 +0200

    Wyswietlanie danych z serwa poczatek pisania kodu do validaci ID

---
+++ Functions/displayOptions.js
@@ -0,0 +1,13 @@
+module.exports = {
+    Startujacy: (req,res) => {
+        const conn = require('./dbconn')
+        const query = "SELECT * FROM startujacy"
+        conn.query(query, (err,row) => {
+            err ? new Error(err) : false
+    
+            res.render('main', {
+                dane: [...row]
+            })
+        })
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 document.querySelector('form').reset()
 
 document.querySelector('#id').addEventListener('blur', e => {
-	
+	e.target.value === false ? alert('Wpisz cos') : alert(e.target.value)
 })
 
